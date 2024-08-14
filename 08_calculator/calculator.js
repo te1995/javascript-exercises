de61322b47db790c502d5d6fdefa0696a00c4156
te1995 <166929277+te1995@users.noreply.github.com>
2024-08-14 19:32:11 +0200

    Calculator exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,40 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(args) {
+  if(args.length === 0) {
+    return 0;
+  }
+  return args.reduce((acc, curr) => acc + parseInt(curr));
 };
 
-const multiply = function() {
+const multiply = function(args) {
+  if(args.length === null) {
+    return 0;
+  }
+
+  return args.reduce(((a, b) => a = a * b));
 
 };
 
-const power = function() {
-	
+const power = function(num, exp) {
+  return num**exp;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	let sum = 1; 
+  while(true) {
+    if(num === 0) {
+      return sum;
+    }
+    sum=sum*num;
+    num--;
+  }
 };
 
 // Do not edit below this line
